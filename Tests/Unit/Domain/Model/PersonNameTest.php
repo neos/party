638ad0e5f091f4e120a2e016761ae0949f5c678c
Robert Lemke <robert@typo3.org>
2010-12-15 13:03:13 +0100

    [~TASK] FLOW3: Use new base test namespace
    This changeset adjusts the base test case class name used in
    the tests of this package to the new namespace.
    
    Relates to: #10438
    
    Change-Id: Ia89df5981e3b2c1395f25d8c25fe6387177ad34e

--- Tests/Unit/Domain/Model/PersonNameTest.php
+++ Tests/Unit/Domain/Model/PersonNameTest.php
@@ -27,7 +27,7 @@ namespace F3\Party\Tests\Unit\Domain\Model;
  *
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  */
-class PersonNameTest extends \F3\Testing\BaseTestCase {
+class PersonNameTest extends \F3\FLOW3\Tests\UnitTestCase {
 
 	/**
 	 * @test
