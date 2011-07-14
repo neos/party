1b7fef9013161d0f9623a47e3162e0f4ae2205ed
Karsten Dambekalns <karsten@typo3.org>
2011-07-14 22:08:12 +0200

    [TASK] Add migration for dtype columns
    Change-Id: Icf670de4b358078b1ce89beb13c05e88d6bd0cda

---
+++ Migrations/Mysql/Version20110714212900.php
@@ -0,0 +1,33 @@
+<?php
+namespace TYPO3\FLOW3\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Update dtype values
+ */
+class Version20110714212900 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("UPDATE party_abstractparty SET dtype=CONCAT('typo3_', SUBSTR(dtype, 4)) WHERE dtype LIKE 'f3_%'");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("UPDATE party_abstractparty SET dtype=CONCAT('f3_', SUBSTR(dtype, 4)) WHERE dtype LIKE 'typo3_%'");
+	}
+}
+
+?>
