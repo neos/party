0814162745e15e5cb0ef6acf6c71e272da2c0e36
Karsten Dambekalns <karsten@typo3.org>
2012-03-21 18:01:37 +0100

    [TASK] Adjust columns to NOT NULL default.
    Change-Id: Id2856243331e3616e4739c63def7db24524624a0
    Related: #35059
    Releases: 1.1

---
+++ Migrations/Mysql/Version20120329220341.php
@@ -0,0 +1,35 @@
+<?php
+namespace TYPO3\FLOW3\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Adjust default values to NOT NULL unless allowed in model.
+ */
+class Version20120329220341 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("ALTER TABLE typo3_party_domain_model_electronicaddress CHANGE identifier identifier VARCHAR(255) NOT NULL, CHANGE approved approved TINYINT(1) NOT NULL");
+		$this->addSql("ALTER TABLE typo3_party_domain_model_personname CHANGE title title VARCHAR(255) NOT NULL, CHANGE firstname firstname VARCHAR(255) NOT NULL, CHANGE middlename middlename VARCHAR(255) NOT NULL, CHANGE lastname lastname VARCHAR(255) NOT NULL, CHANGE othername othername VARCHAR(255) NOT NULL, CHANGE alias alias VARCHAR(255) NOT NULL, CHANGE fullname fullname VARCHAR(255) NOT NULL");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("ALTER TABLE typo3_party_domain_model_electronicaddress CHANGE identifier identifier VARCHAR(255) DEFAULT NULL, CHANGE approved approved TINYINT(1) DEFAULT NULL");
+		$this->addSql("ALTER TABLE typo3_party_domain_model_personname CHANGE title title VARCHAR(255) DEFAULT NULL, CHANGE firstname firstname VARCHAR(255) DEFAULT NULL, CHANGE middlename middlename VARCHAR(255) DEFAULT NULL, CHANGE lastname lastname VARCHAR(255) DEFAULT NULL, CHANGE othername othername VARCHAR(255) DEFAULT NULL, CHANGE alias alias VARCHAR(255) DEFAULT NULL, CHANGE fullname fullname VARCHAR(255) DEFAULT NULL");
+	}
+}
+
+?>
