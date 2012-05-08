33a4feec3066851329ab21c27bbace905b7363bf
Aske Ertmann <aske@moc.net>
2012-05-08 20:22:37 +0200

    [TASK] Adjust controller FLOW3 namespace from MVC to Mvc
    Change-Id: I9632bd7ada14e4e157e916d0d10c8404bf272c99

--- Classes/Controller/SetupController.php
+++ Classes/Controller/SetupController.php
@@ -16,7 +16,7 @@ use TYPO3\FLOW3\Annotations as FLOW3;
 /**
  * @FLOW3\Scope("singleton")
  */
-class SetupController extends \TYPO3\FLOW3\MVC\Controller\ActionController {
+class SetupController extends \TYPO3\FLOW3\Mvc\Controller\ActionController {
 
 	/**
 	 * The authentication manager
