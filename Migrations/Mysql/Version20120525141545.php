735ccefcaf41c46f60cc2327a7ca259e22f34d6b
Karsten Dambekalns <karsten@typo3.org>
2012-05-25 14:29:09 +0200

    [TASK] Make usagetype on ElectronicAddress nullable
    The usage property has no default value and thus should be
    nullable to make "any" usage possible.
    
    Adds a functional test (thanks Adrian Föder) as well.
    
    Change-Id: Ie586640347b87daaf0c0e7612c8215d58d61b705
    Related: #35059
    Releases: 1.1, 1.2

---
+++ Migrations/Mysql/Version20120525141545.php
@@ -0,0 +1,33 @@
+<?php
+namespace TYPO3\FLOW3\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Make usagetype of ElectronicAddress nullable
+ */
+class Version20120525141545 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("ALTER TABLE typo3_party_domain_model_electronicaddress CHANGE usagetype usagetype VARCHAR(20) DEFAULT NULL");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("ALTER TABLE typo3_party_domain_model_electronicaddress CHANGE usagetype usagetype VARCHAR(20) NOT NULL");
+	}
+}
+
+?>
---
+++ Migrations/Postgresql/Version20120525141546.php
@@ -0,0 +1,33 @@
+<?php
+namespace TYPO3\FLOW3\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Make usagetype of ElectronicAddress nullable
+ */
+class Version20120525141546 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql");
+
+		$this->addSql("ALTER TABLE typo3_party_domain_model_electronicaddress ALTER usagetype DROP NOT NULL");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql");
+
+		$this->addSql("ALTER TABLE typo3_party_domain_model_electronicaddress ALTER usagetype SET NOT NULL");
+	}
+}
+
+?>
