d79cb768ef8cd4f2ba21678a14cfd2e3eb47f95a
Christian Müller <christian@kitsunet.de>
2014-08-05 10:11:56 +0200

    [BUGFIX] Check for PHP SAPI and version
    Currently the requirement check tries to detect the PHP version
    this could fail early if the found php binary is not CLI SAPI.
    This change first checks for the SAPI and then the version.
    
    Releases: master, 1.0
    Resolves: NEOS-269
    Change-Id: I48dc1a2b59cc870940d5a12d12febbde4196e50a
    Reviewed-on: http://review.typo3.org/32207
    Reviewed-by: Aske Ertmann <aertmann@gmail.com>
    Tested-by: Aske Ertmann <aertmann@gmail.com>

--- Classes/TYPO3/Setup/Core/RequestHandler.php
+++ Classes/TYPO3/Setup/Core/RequestHandler.php
@@ -134,7 +134,7 @@ class RequestHandler extends \TYPO3\Flow\Http\RequestHandler {
 			}
 			return TRUE;
 		} else {
-			return new \TYPO3\Flow\Error\Error('The path to your PHP binary could not be detected. Please set it manually in Configuration/Settings.yaml.', 1341499159, array(), 'Environment requirements not fulfilled');
+			return new \TYPO3\Flow\Error\Error('The path to your PHP (cli) binary could not be detected. Please set it manually in Configuration/Settings.yaml.', 1341499159, array(), 'Environment requirements not fulfilled');
 		}
 	}
 
@@ -152,8 +152,13 @@ class RequestHandler extends \TYPO3\Flow\Http\RequestHandler {
 			} else {
 				$phpCommand = escapeshellarg(Files::getUnixStylePath($phpBinaryPathAndFilename));
 			}
-			$phpVersion = trim(exec($phpCommand . ' -r "echo PHP_VERSION;"'));
-			if ($phpVersion === PHP_VERSION) {
+
+			exec($phpCommand . ' -v', $phpVersionString);
+			if (!isset($phpVersionString[0]) || strpos($phpVersionString[0], '(cli)') === FALSE) {
+				return new \TYPO3\Flow\Error\Error('The specified path to your PHP binary (see Configuration/Settings.yaml) is incorrect or not a PHP command line (cli) version.', 1341839376, array(), 'Environment requirements not fulfilled');
+			}
+			$versionStringParts = explode(' ', $phpVersionString[0]);
+			if (isset($versionStringParts[1]) && trim($versionStringParts[1]) === PHP_VERSION) {
 				return TRUE;
 			}
 		}
