4cb89c6b4692ac8792992ebdcfe96fa16284e3a9
Karsten Dambekalns <karsten@typo3.org>
2015-03-09 21:05:18 +0100

    [TASK] Add "cleanup" schema migrations
    This adds migrations for MySQL and PostgreSQL that tweak the DB schema a
    bit, cleaning up some leftover discrepancies.
    
    Change-Id: Ib4c5b3991fb749350439a5a1d34aa3c21e818949
    Releases: master, 3.0

---
+++ Migrations/Postgresql/Version20150309184457.php
@@ -0,0 +1,31 @@
+<?php
+namespace TYPO3\Flow\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Adjust DB schema to a clean state (remove cruft that built up in the past)
+ */
+class Version20150309184457 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql");
+
+		$this->addSql("CREATE UNIQUE INDEX UNIQ_1EEEBC2F58842EFC ON typo3_party_domain_model_abstractparty_accounts_join (flow_security_account)");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql");
+
+		$this->addSql("DROP INDEX UNIQ_1EEEBC2F58842EFC");
+	}
+}
