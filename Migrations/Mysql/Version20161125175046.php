c22ad319e0723da5bb79b2800bdb3921d1bff053
Christian Mueller <christian@flownative.com>
2016-11-25 17:56:23 +0100

    TASK: Adapt dtype for party single table inheritance
    Changes dtype from typo3_* to neos_* in:
    
    - neos_party_domain_model_personname
    - neos_party_domain_model_electronicaddress
    - neos_party_domain_model_abstractparty

---
+++ Migrations/Mysql/Version20161125175046.php
@@ -0,0 +1,40 @@
+<?php
+namespace Neos\Flow\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Rename dtypes in
+ * - neos_party_domain_model_personname
+ * - neos_party_domain_model_electronicaddress
+ * - neos_party_domain_model_abstractparty
+ */
+class Version20161125175046 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     * @return void
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on "mysql".');
+
+        $this->addSql("UPDATE neos_party_domain_model_personname SET dtype = REPLACE(dtype, 'typo3_', 'neos_')");
+        $this->addSql("UPDATE neos_party_domain_model_electronicaddress SET dtype = REPLACE(dtype, 'typo3_', 'neos_')");
+        $this->addSql("UPDATE neos_party_domain_model_abstractparty SET dtype = REPLACE(dtype, 'typo3_', 'neos_')");
+    }
+
+    /**
+     * @param Schema $schema
+     * @return void
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on "mysql".');
+
+        $this->addSql("UPDATE neos_party_domain_model_personname SET dtype = REPLACE(dtype, 'neos_', 'typo3_')");
+        $this->addSql("UPDATE neos_party_domain_model_electronicaddress SET dtype = REPLACE(dtype, 'neos_', 'typo3_')");
+        $this->addSql("UPDATE neos_party_domain_model_abstractparty SET dtype = REPLACE(dtype, 'neos_', 'typo3_')");
+    }
+}
